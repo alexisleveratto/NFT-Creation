8f221cdee19b3dcafc8c1dbf11365ee7d407dd3a
alexisleveratto <alexis.leveratto@gmail.com>
2022-06-28 11:32:24 +0100

    Refactor filesystem

--- transformation/px_images.py
+++ transformation/px_images.py
@@ -20,31 +20,33 @@ class PixelateImage:
     def pixelate_image(self):
         for count, image_name in enumerate(os.listdir(self._images_folder)):
 
+            if "DONE" in image_name:
+                print("[INFO] - Ignoring {}".format(image_name))
+            else:
+                print("[INFO] - Pixelating {}".format(image_name))
+                # Construct Image path
+                image_path = "{}/{}".format(self._images_folder, image_name)
 
-            print("[INFO] - Pixelating {}".format(image_name))
-            # Construct Image path
-            image_path = "{}/{}".format(self._images_folder, image_name)
+                # Read Image
+                image = cv2.imread(image_path)
 
-            # Read Image
-            image = cv2.imread(image_path)
+                # Get input size
+                height, width = image.shape[:2]
 
-            # Get input size
-            height, width = image.shape[:2]
+                # Resize input to "pixelated" size
+                temp = cv2.resize(image, (self._px_w, self._px_h), interpolation=cv2.INTER_LINEAR)
 
-            # Resize input to "pixelated" size
-            temp = cv2.resize(image, (self._px_w, self._px_h), interpolation=cv2.INTER_LINEAR)
+                # Initialize output image
+                output = cv2.resize(temp, (width, height), interpolation=cv2.INTER_NEAREST)
 
-            # Initialize output image
-            output = cv2.resize(temp, (width, height), interpolation=cv2.INTER_NEAREST)
+                # Construct Output path
+                output_path = "{}/results_{}.jpg".format(self._results_folder, count)
 
-            # Construct Output path
-            output_path = "{}/results_{}.jpg".format(self._results_folder, count)
+                # Save "pixelated" image
+                cv2.imwrite(output_path, output)
 
-            # Save "pixelated" image
-            cv2.imwrite(output_path, output)
-
-            # Mark as done
-            new_image_name = mark_as_done(image_path)
-            os.rename(image_path, new_image_name)
+                # Mark as done
+                new_image_name = mark_as_done(image_path)
+                os.rename(image_path, new_image_name)
 
         print('[INFO] - Finish pixelating')
